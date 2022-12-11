d7f50719e0a72b78f08d351867901df282c4159b
jamiu <mjustice4uall90.com>
2022-12-11 13:23:01 -0800

    TASK 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,14 +17,12 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) ||
-						       	(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' &&
-								tens == '8')) /*adds commas and spaces*/
+					if (!(ones == '9' && hundreds == '7' && tens == '8')) /*adds commas and spaces*/
 					{
 						putchar(',');
 						putchar(' ');
@@ -36,4 +34,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-					
