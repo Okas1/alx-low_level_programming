f616cff59d34ba4cc758eb8c655f5ff9fab18663
jamiu <mjustice4uall90.com>
2022-12-13 08:20:34 -0800

    TASK 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+##include "main.h"
+
+/**
+ * print _alphabet - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
