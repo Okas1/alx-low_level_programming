548a5e4d69e33c498eb1c13d6899460697eb5c18
jamiu <mjustice4uall90.com>
2022-12-14 16:54:08 -0800

    TASK 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - Return 1 if c is a letter. lowercase or uppercase
- * @c: unsigned int value to compared with ASCII value
+ * @c: unsigned int value to be compared with ASCII value
  * Return: 1 if its alphabet otherwise 0
  */
 
 int _isalpha(int c)
 {
 	if (c >= 65 && c <= 122)
-		return (1),
+		return (1);
 	else
 		return (0);
 }
