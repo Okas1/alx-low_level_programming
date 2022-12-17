d9a1a3281c21b5af92bd6fb7e7a22e61ab05799f
jamiu <mjustice4uall90.com>
2022-12-17 13:19:16 -0800

    TASK 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,8 +5,8 @@
  * main - Entry point
  *
  * Description: computes the sum of all
- *              the multiples of 3 or 5
- *              below 1024 (excluded)
+ * the multiples of 3 0r 5
+ * below 1024 (excluded)
  *
  * Return: Always 0 (success)
  */
@@ -15,7 +15,7 @@ int main(void)
 
 {
 	int sum, num;
-
+	
 	for (num = 0; num < 1024; ++num)
 	{
 		if ((num % 3 == 0) || (num % 5 == 0))
