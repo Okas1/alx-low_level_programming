e653cd52ac07dae10b65c484018392da466c6b82
jamiu <mjustice4uall90.com>
2022-12-17 10:52:26 -0800

    ADV TASK 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,17 +1,17 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - print prime
  * Return: 0
  */
-int main(video)
+int main(void)
 {
 	unsigned long int i = 3, n = 612852475143;
 
 	for (; i < 12057; i += 2)
 	{
 		while (n % i == 0 && n != i)
-			n /=i;
+			n /= i;
 	}
 	printf("%lu\n", n);
 	return (0);
