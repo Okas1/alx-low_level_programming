4d1ff42db8f0a99406d860a28a602a9b57df5507
jamiu <mjustice4uall90.com>
2022-12-17 14:44:25 -0800

    TASK 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all possible different combinations of two digits
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  *
  */
 
@@ -12,8 +12,8 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-
-	for (tens = '0';  tens <= '9'; ones++)/* prints ones digit*/
+	
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
@@ -21,7 +21,7 @@ int main(void)
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				if (!(ones == '9' && tens == '8'))/*addes commma and space*/
 				{
 					putchar(',');
 					putchar(' ');
@@ -29,7 +29,7 @@ int main(void)
 			}
 		}
 	}
-
+	
 	putchar('\n');
 
 	return (0);
