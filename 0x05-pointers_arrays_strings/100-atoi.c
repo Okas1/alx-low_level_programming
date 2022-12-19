ed7b2fbeca66819cf4a67232bd26bf55a8178269
jamiu <mjustice4uall90.com>
2022-12-19 14:12:00 -0800

    ADV TASK 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,12 +9,15 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+	
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
+		
 		if (*(s + count) == '-')
 			k *= -1;
+		
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
@@ -23,6 +26,7 @@ int _atoi(char *s)
 		}
 		count++;
 	}
+	
 	for (i = count - size; i < count; i++)
 	{
 		j = j + ((*(s + i) - 48) * m);
