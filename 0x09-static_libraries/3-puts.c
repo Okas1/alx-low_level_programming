4aec8b386cedbc4ce44926ee76935c8c322779ac
jamiu <mjustice4uall90.com>
2023-01-11 17:48:37 -0800

    My Task

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+* main - Entry point
+* _puts - print a string to stdout
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
