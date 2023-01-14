b5edd6df26411555dc7ec11b8895af43fdca96ea
jamiu <mjustice4uall90.com>
2023-01-14 07:39:58 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,14 +11,14 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	
 	int size = n;
-	
+
 	if (size > 0)
 	{
 		int i;
-		
+
 		for (i = 0; i < size; i++)
 		dest[i] = src[i];
 	}
-	
+
 	return (dest);
 }
