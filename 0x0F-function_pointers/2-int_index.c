5fa73da9370be01ee64a2027d3fe3f4a431ce4cb
jamiu <mjustice4uall90.com>
2023-01-18 05:30:27 -0800

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))return (i);
+		if (cmp(array[i]))
+			return (i);
 	}
 
 	return (-1);
